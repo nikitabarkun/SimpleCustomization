e72ad4e118c12ba6758fd01b79343fa3aeddc752
nbarkun <nikitaba@playtika.com>
2020-11-12 18:27:30 +0300

    Added project files

---
+++ Assets/Scripts/CharacterSelectionButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectionButton : MonoBehaviour
+{
+    public static string selectedCharacter;
+    
+    [SerializeField] private string characterName;
+    [SerializeField] private GameObject characterRoot;
+        
+    [SerializeField] private GameObject characterLegs;
+    [SerializeField] private GameObject characterTorso;
+    [SerializeField] private GameObject characterHead;
+    [SerializeField] private GameObject characterHair;
+    
+    [SerializeField] private GameObject customizationMenuPanel;
+    
+    public void LoadCharacter()
+    {
+        var character = Resources.Load<GameObject>($"{characterName}");
+        Instantiate(character, characterRoot.transform);
+
+        selectedCharacter = characterName;
+        
+        transform.parent.gameObject.SetActive(false);
+        customizationMenuPanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/CustomizationApplyingButton.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Linq;
+using CartoonHeroes;
+using UnityEngine;
+
+public class CustomizationApplyingButton : MonoBehaviour
+{
+    [SerializeField] private GameObject characterRoot;
+    [SerializeField] private GameObject targetBodyPart;
+    
+    [SerializeField] private string targetBodyPartString;
+    [SerializeField] private string textureIndex;
+    
+    private void OnEnable()
+    {
+        targetBodyPart = characterRoot.GetComponentInChildren<SetCharacter>().itemGroups
+            .First(x => x.name.Contains(targetBodyPartString)).items[0].prefab;
+    }
+
+    public void OnClick()
+    {
+        var loadedTextureName = $"{CharacterSelectionButton.selectedCharacter}_{targetBodyPartString}_{textureIndex}.tga";
+        
+        var directoryInfo = new DirectoryInfo(Application.streamingAssetsPath);
+        var allFiles = directoryInfo.GetFiles("*.*.*.*", SearchOption.AllDirectories);
+
+        foreach (var VARIABLE in allFiles)
+        {
+            print(VARIABLE.Name);
+        }
+        
+        var fileInfo = allFiles.FirstOrDefault(x =>
+            x.Name == loadedTextureName);
+        print(loadedTextureName);
+        print(fileInfo.Name);
+        
+        //var bytes = File.ReadAllBytes($"{CharacterSelectionButton.selectedCharacter}/{targetBodyPartString}fileInfo.Name);
+        var texture2D = new Texture2D(1, 1);
+        //texture2D.LoadImage(bytes);
+
+        targetBodyPart.GetComponent<Renderer>().material.mainTexture = texture2D;
+    }
+}
